df01cb24066292920b2445b3ee6123880aa2fdb6
Israel Lopez <israel.lpz.18@gmail.com>
2023-11-15 23:42:13 -0600

    Add servers

--- Parcial3/ServerExpress/main.js
+++ Parcial3/ServerExpress/main.js
@@ -1,20 +1,30 @@
 import express from 'express';
 import morgan from 'morgan';
+import {createConnection} from 'mysql2/promise';
 
 const app = express();
 app.use(morgan("dev"))
 
-app.get('/clientes', (req, res) => {
-    console.log(req.query);
-    res.json({message: 'Server exoress GET'});
+const conn = await createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'admin',
+    port: 3306,
+    database: 'school'
+})
+
+app.get('/users', async (req, res) => {
+    let where = '';
+    if(typeof req.query.userId !== 'undefined'){
+        where = ` where id = ${req.query.userId}`;
+    }
+
+    const [results] = await conn.query(`SELECT * FROM users ${where}`);
+    if(results.length === 0){
+        return res.json({message: 'No existe'})
+    }
+    res.json(results[0]);
 });
 
-app.post('/clientes', (req, res) => {
-    res.json({message: 'Server express POST'})
-});
-
-app.delete('/clientes', (req, res) => {
-    res.json({message: 'Server express DELETE'})
-});
 
 app.listen(5000, () => console.log('Running server'));
